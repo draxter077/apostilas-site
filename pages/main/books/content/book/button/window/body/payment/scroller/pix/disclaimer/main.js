66337c01e3bde77a91479a601869028d438b4240
philippe <philippe.idalgoprestes@gmail.com>
2025-08-19 12:59:28 -0300

    m

--- pages/main/books/content/book/button/window/body/payment/scroller/pix/disclaimer/main.js
+++ pages/main/books/content/book/button/window/body/payment/scroller/pix/disclaimer/main.js
@@ -12,6 +12,6 @@ export default function disclaimer(){
         }`
 
     const disclaimer = cE("div", style)
-    disclaimer.innerHTML = "Aqui estão as instruções de pagamento. Elas também foram enviadas pelo seu e-mail.<br>Em até 24h úteis após o pagamento, você receberá por e-mail a sua apostila (verifique seu spam)!<br>Quaisquer dúvidas, entre em contato pelos links que estão disponíveis ao longo dessa página e no e-mail enviado."
+    disclaimer.innerHTML = "Aqui estão as instruções de pagamento PIX, que também foram enviadas pelo seu e-mail.<br>Em até 24h úteis após o pagamento, você receberá a sua apostila pelo e-mail (verifique seu spam!). Caso algo dê errado, entraremos em contato via WhatsApp ou e-mail.<br>Quaisquer dúvidas, entre em contato pelos links que estão disponíveis ao longo dessa página e no e-mail enviado."
     return(disclaimer)
 }
