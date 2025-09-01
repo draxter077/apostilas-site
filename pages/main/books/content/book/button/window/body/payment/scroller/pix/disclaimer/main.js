4ad3099ffbf3a1d319837e85b4f57ffb77da132d
philippe <philippe.idalgoprestes@gmail.com>
2025-09-01 12:58:33 -0300

    discalmeri spp

--- pages/main/books/content/book/button/window/body/payment/scroller/pix/disclaimer/main.js
+++ pages/main/books/content/book/button/window/body/payment/scroller/pix/disclaimer/main.js
@@ -12,6 +12,6 @@ export default function disclaimer(){
         }`
 
     const disclaimer = cE("div", style)
-    disclaimer.innerHTML = "Aqui estão as instruções de pagamento PIX, que também foram enviadas pelo seu e-mail. Aponte sua câmera para o QR Code ou copie e cole o código PIX abaixo no aplicativo do seu banco.<br>Em até 24h úteis após o pagamento, você receberá a sua apostila pelo e-mail (verifique seu spam!). Caso algo dê errado, entraremos em contato via WhatsApp ou e-mail.<br>Quaisquer dúvidas, entre em contato pelos links que estão disponíveis ao longo dessa página e no e-mail enviado."
+    disclaimer.innerHTML = "Aqui estão as instruções de pagamento PIX, que também foram enviadas para seu e-mail (lembre de verificar sua caixa de spam). Aponte sua câmera para o QR Code ou copie e cole o código PIX abaixo no aplicativo do seu banco.<br>Em até 24 horas após o pagamento, você receberá a sua apostila em seu WhatsApp.<br>Muito obrigado pela sua compra!<br>Esperamos que tenha tido uma ótima experiência<br>Quaisquer dúvidas, reclamações ou sugestões, entre em contato pelos links que estão disponíveis ao longo dessa página e no e-mail enviado."
     return(disclaimer)
 }
